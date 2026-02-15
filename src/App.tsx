ad38702e640d97cd063c594d111acd939e69ee0f
henrique <wzhenrique@proton.me>
2026-02-15 03:20:56 -0300

    feat: fetch and render ditto data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+
 export default function App() {
+  const [pokemon, setPokemon] = useState<any>(null)
+
+  useEffect(() => {
+    async function carregarDados() {
+      const resposta = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      const dados = await resposta.json()
+      
+      setPokemon(dados)
+    }
+
+    carregarDados()
+  }, [])
+
   return (
-    <div className="flex h-screen items-center justify-center bg-zinc-900">
-      <h1 className="text-4xl font-bold text-white">
-        Hello World! 🌎
-      </h1>
+    <div className="flex flex-col h-screen items-center justify-center bg-zinc-900 text-white">
+      {pokemon && (
+        <>
+          <h1 className="text-4xl font-bold capitalize">
+            {pokemon.name}
+          </h1>
+          <img 
+            src={pokemon.sprites.front_default} 
+            alt={pokemon.name} 
+            className="w-32 h-32"
+          />
+        </>
+      )}
     </div>
   )
 }
